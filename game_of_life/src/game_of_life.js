9083d88b4b9f95eab12f6311ad6e3dd4a398ed93
Deniel Horvatic <deniel.horvatic@porschedigitallab.com>
2018-08-28 17:04:40 +0200

    Added test files for field and constructor

--- game_of_life/src/game_of_life.js
+++ game_of_life/src/game_of_life.js
@@ -1,22 +1,33 @@
 class GameOfLife {
   constructor (height, width) {
+    if (typeof height !== 'number' || typeof width !== 'number') {
+      throw new Error('Height and width are not set as numbers')
+    }
+    if (height <= 0 || width <= 0) {
+      throw new Error('Height and width should be at least 1')
+    }
     this.height = height
     this.width = width
     this.field = []
-    this.initializeField()
+    this.field = this.initializeField()
+    this.state = 0
   }
 
   initializeField () {
     let field = []
-    for (let h = 0; h < this.height; h++) {
-      for (let w = 0; w < this.width; w++) {
-        if (w === 0) {
+    for (let height = 0; height < this.height; height++) {
+      for (let width = 0; width < this.width; width++) {
+        if (width === 0) {
           field.push([])
         }
-        field[h].push(false)
+        field[height].push(false)
       }
     }
-    this.field = field
+    return field
+  }
+
+  swtichToNextState () {
+    this.state += 1
   }
 }
 
--- game_of_life/test/game_of_life.js
+++ game_of_life/test/game_of_life.js
@@ -1,15 +1,92 @@
+/* global describe, it, beforeEach */
+
 var expect = require('chai').expect
+var should = require('chai').should()
 var GameOfLife = require('../src/game_of_life')
 
-describe ('Game of Life', () => {
-  describe ('Precondition', () => {
-    it('creates initial empty array', () => {
-      const gameOfLife = new GameOfLife(2, 2)
+describe('Game of Life', () => {
+  let gameOfLife
+  beforeEach(() => {
+    gameOfLife = new GameOfLife(2, 2)
+  })
 
+  describe('Space', () => {
+    it('should creates initial empty two dimensionaly array', () => {
       const space = gameOfLife.field
-
       const result = [ [ false, false ], [ false, false ] ]
       expect(space).deep.equal(result)
+    })
+    it('should throw an error if negative numbers are entered', () => {
+      try {
+        gameOfLife = new GameOfLife(0, -1)
+      } catch (err) {
+        should.exist(err)
+      }
+    })
+    it('should throw an error if nothing are entered', () => {
+      try {
+        gameOfLife = new GameOfLife()
+      } catch (err) {
+        should.exist(err)
+      }
+    })
+    it('should throw an error if strings are entered', () => {
+      try {
+        gameOfLife = new GameOfLife('a', 'sadsad')
+      } catch (err) {
+        should.exist(err)
+      }
+    })
+  })
+  describe('State', () => {
+    let state
+    it('should initialize with state zero', () => {
+      state = gameOfLife.state
+      expect(state).to.equal(0)
+    })
+    it('should change to next state', () => {
+      gameOfLife.swtichToNextState()
+      state = gameOfLife.state
+      expect(state).to.equal(1)
+    })
+  })
+  describe('Cell', () => {
+    it('should place a cell on any field', () => {
+
+    })
+    xit('should change dead cell to live cell', () => {
+      const cellBefore = gameOfLife.field[0][0]
+      expect(cellBefore.state).to.equal('DEAD')
+      expect(cell.state).to.equal('LIVE')
+    })
+    xit('should change live cell to dead cell', () => {
+      expect(cellBefore).to.equal('LIVE')
+      expect(cell).to.equal('DEAD')
+    })
+  })
+  describe('Game rules', () => {
+    xit('Any live cell with fewer than two live neighbors dies, as if by under population.', () => {
+      expect(cellBefore).to.equal('LIVE')
+      expect(zeroNeighbour).to.equal(0)
+      expect(oneNeighbour).to.equal(1)
+      expect(cell).to.equal('DEAD')
+    })
+    xit('Any live cell with two or three live neighbors lives on to the next generation', () => {
+      expect(cellBefore).to.equal('LIVE')
+      expect(twoNeuighbour).to.equal(2)
+      expect(threeNeighbour).to.equal(3)
+      expect(cell).to.equal('LIVE')
+    })
+    xit('Any live cell with more than three live neighbors dies, as if by overpopulation.', () => {
+      expect(cellBefore).to.equal('LIVE')
+      expect(fourNeighbour).to.equal(4)
+      expect(fourNeighbour).to.equal(8)
+      expect(cell).to.equal('DEAD')
+    })
+    xit('Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.', () => {
+      expect(cellBefore).to.equal('DEAD')
+      expect(fourNeighbour).to.equal(3)
+      expect(cell).to.equal('LIVE')
     })
   })
 })
