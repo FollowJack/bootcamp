0d92caa4f8605c1df62a4bb85ce015b03ac69e53
FollowJack <d.horvatic@outlook.com>
2018-11-18 22:30:49 +0100

    Added new list of review videos

--- talk_to_me/talk_to_me/screens/LibraryScreen.js
+++ talk_to_me/talk_to_me/screens/LibraryScreen.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import Touchable from 'react-native-platform-touchable';
 
 export default class LibraryScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Library',
   };
 
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        <View>
+          <Text style = {styles.optionsTitleText}>Reviews
+          </Text>
+          <Touchable
+            style={styles.option}
+            background={Touchable.Ripple('#ccc', false)}>
+            <View style={{ flexDirection: 'row' }}>
+              <View style={styles.optionTextContainer}>
+                <Text style={styles.optionText}>
+                  Review 2018.11.18
+                </Text>
+              </View>
+            </View>
+          </Touchable>
+        </View>
       </ScrollView>
     );
   }
@@ -23,5 +35,25 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 15,
     backgroundColor: '#fff',
+  },
+  optionsTitleText:{
+    fontSize:16,
+    marginLeft:15,
+    marginTop:9,
+    marginBottom:12
+  },
+  optionIconContainer: {
+    marginRight: 9,
+  },
+  option: {
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#EDEDED',
+  },
+  optionText: {
+    fontSize: 15,
+    marginTop: 1,
   },
 });
