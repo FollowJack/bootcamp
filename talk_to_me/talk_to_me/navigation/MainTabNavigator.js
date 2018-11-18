dc574f4df5c0b1662b885a84969256aed902e5f4
FollowJack <d.horvatic@outlook.com>
2018-11-18 21:04:36 +0100

    Changed navigation buttons names

--- talk_to_me/talk_to_me/navigation/MainTabNavigator.js
+++ talk_to_me/talk_to_me/navigation/MainTabNavigator.js
@@ -12,7 +12,7 @@ const HomeStack = createStackNavigator({
 });
 
 HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
+  tabBarLabel: 'Action',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
@@ -30,7 +30,7 @@ const LinksStack = createStackNavigator({
 });
 
 LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+  tabBarLabel: 'Library',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
@@ -44,7 +44,7 @@ const SettingsStack = createStackNavigator({
 });
 
 SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
+  tabBarLabel: 'Configure',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
